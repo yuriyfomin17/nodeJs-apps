86fc38bf7d9a1b08ba00f16d9550f7e46ffb5dfd
yuriyfomin17 <fominyura2012@gmail.com>
2020-09-05 21:43:07 +0300

    Apps added

---
+++ web-server/src/app.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+
+
+const app = express()
+
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: "cloudy",
+        weather: 28
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
